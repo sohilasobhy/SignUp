0f734d828b3d1273e0d78329dc95f68ffe8b7c9f
Admin <sohilasobhysadek@gmail.com>
2025-03-26 22:45:23 +0200

    edits

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ document.getElementById("nextStep1").addEventListener("click", function () {
 document.getElementById("prevStep1").addEventListener("click", function () {
     var phoneTab = new bootstrap.Tab(document.querySelector('a[href="#phone"]'));
     phoneTab.show();
+    let step1 = document.getElementById("step1")
+    step1.classList.remove("done")
 });
 
 document.getElementById("nextStep2").addEventListener("click", function () {
@@ -23,6 +25,8 @@ document.getElementById("nextStep2").addEventListener("click", function () {
 document.getElementById("prevStep2").addEventListener("click", function () {
     var yourselfTab = new bootstrap.Tab(document.querySelector('a[href="#yourself"]'));
     yourselfTab.show();
+    let step2 = document.getElementById("step2")
+    step2.classList.remove("done")
 });
 document.getElementById("nextStep3").addEventListener("click", function () {
     var teamMemberTab = new bootstrap.Tab(document.querySelector('a[href="#teamMember"]'));
@@ -33,6 +37,8 @@ document.getElementById("nextStep3").addEventListener("click", function () {
 document.getElementById("prevStep3").addEventListener("click", function () {
     var companyTab = new bootstrap.Tab(document.querySelector('a[href="#company"]'));
     companyTab.show();
+    let step3 = document.getElementById("step3")
+    step3.classList.remove("done")
 });
 
 document.getElementById("nextStep4").addEventListener("click", function () {
